2a75731ca3297fe7af696dcf60a88b54d1511914
codyooten <aco0002@mix.wvu.edu>
2020-09-22 22:05:13 -0400

    Lab 4

---
+++ about.php
@@ -0,0 +1,46 @@
+<?php
+require "includes/header.php"
+?>
+<main>
+<link rel="stylesheet" href="css/about.css">
+<div class = "bg-cover">
+	<div class = "About">
+	<h1 style ="text-align: center; color: rgb(255, 0, 0)"> About Us Page </h1>
+	<p style = "text-align: center; color: rgb(255, 0, 0)"> We intellectuals strive in providing great movie reviews so you have something enjoyable to watch during this pandemic and also might introduce you to movie genres you thought you would never like. We formed this group to save the world from the dreaded evil known as boredom. </p>
+	</div>
+<h2 style="text-align: center; color: red"> THE BOYS </h2>
+<div class = "members">
+	<div class = "row">
+		<div class = "column">
+			<img src = "images/Bobross.jpg" alt = "Bob Ross" style = "width:200px; height:150px">
+			<h2 style = "color:rgb(0, 0, 0)"> Bob Ross </h2>
+		<p class = "title" p style = "color: rgb(0, 0, 0)"> Art Expert </p>
+		<p style = "color: rgb(0, 0, 0)"> ManBearPig@algore.com </p>
+		<p> <button class = "button"> Contact Me </button></p>
+	</div>
+	<div class = "column">
+		<img src = "images/Tonystark.jpg" alt = "Tony Stark" style = "width:200px; height:150px">
+			<h2 style = "color:rgb(255, 0, 0)"> Tony Stark </h2>
+			<p class = "title" p style = "color: rgb(255, 0, 0)"> CEO </p>
+			<p style = "color:rgb(255, 0, 0)"> IamaGenius3000@avengers.com </p>
+		<p> <button class = "button"> Contact Me </button></p>
+	</div>
+		<div class = "column">
+		<img src = "images/Ozzy.jpg" alt = "Ozzy" style = "width:200px; height:150px">
+			<h2 style = "color:rgb(0, 0, 0)"> Ozzy Osbourne </h2>
+			<p class = "title" style = "color:rgb(0, 0, 0)"> Music Expert </p>
+			<p style = "color: rgb(0, 0, 0)"> IeatBats54@ozzysboneyard.com </p>
+		<p> <button class = "button"> Contact Me </button></p>
+	</div>
+	<div class = "column">
+		<img src = "images/Tyler1.png" alt = "Tyler" style = "width:200px; height:150px">
+			<h2 style = "color:rgb(255, 0, 0)"> Tyler1 </h2>
+			<p class = "title" p style = "color: rgb(255, 0, 0)"> Hype Man </p>
+			<p style = "color: rgb(255, 0, 0)"> Iamthegreatestever2012@tyler1fanpage.com </p>
+		<p> <button class = "button"> Contact Me </button></p>
+	</div>
+</div>
+</div>
+</div>
+</div>
+</main>
